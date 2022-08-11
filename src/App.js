a1ec410a1a665d0e112c66ab2159fd022b2f0522
ben40d9 <benpaley6@gmail.com>
2022-08-10 23:22:46 -0700

    break point, testing other app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import "./App.css";
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 
 const App = () => {
-  const [advice, setAdvice] = useState("");
-
   useEffect(() => {
-    const url = "https://api.adviceslip.com/advice";
+    const url =
+      "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js";
 
     const fetchData = async () => {
       try {
         const response = await fetch(url);
         const data = await response.json();
         console.log(data);
-        setAdvice(data.slip.advice);
       } catch (err) {
         console.log("error", err);
       }
@@ -20,11 +18,7 @@ const App = () => {
 
     fetchData();
   }, []);
-  return (
-    <div>
-      <p>{advice}</p>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
