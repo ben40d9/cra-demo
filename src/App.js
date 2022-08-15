c4cc936aeccfc9cf86921f0f42e4ea44a9e93195
ben40d9 <benpaley6@gmail.com>
2022-08-15 00:14:20 -0700

    set up new approach for tomorrow, imported useState & am going to manipulate DOM based on state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import "./App.css";
-import React, { useEffect, ReactDOM } from "react";
-import { createRoot } from "react-dom/client";
+import React, { useEffect, useState } from "react";
 
 const App = () => {
+  const [advice, setAdvice] = useState("");
+
   useEffect(() => {
     //adding the ?_limit=X to the end of the url sets a limit on how many objects you get back
     const url = "https://jsonplaceholder.typicode.com/posts?_limit=12";
@@ -16,11 +17,11 @@ const App = () => {
         //logging (...data) show objects listed individually
         // console.log(...data);
 
-        //using array manipuation, get params of each object in the array
-        const dataTitle = await data.map((objs) => {
-          return objs.title;
-        });
-        console.log(dataTitle);
+        // //using array manipuation, get params of each object in the array
+        // const dataTitle = await data.map((objs) => {
+        //   return objs.title;
+        // });
+        // console.log(dataTitle);
       } catch (err) {
         console.log("error", err);
       }
@@ -28,40 +29,7 @@ const App = () => {
 
     fetchData();
   }, []);
-  function Blog(props) {
-    const sidebar = (
-      <ul>
-        {props.posts.map((post) => (
-          <li key={post.id}>{post.title}</li>
-        ))}
-      </ul>
-    );
-    const content = props.posts.map((post) => (
-      <div key={post.id}>
-        <h3>{post.title}</h3>
-        <p>{post.content}</p>
-      </div>
-    ));
-    return (
-      <div>
-        {sidebar}
-        <hr />
-        {content}
-      </div>
-    );
-  }
-
-  const posts = [
-    { id: 1, title: "Hello World", content: "Welcome to learning React!" },
-    {
-      id: 2,
-      title: "Installation",
-      content: "You can install React from npm.",
-    },
-  ];
-
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(<Blog posts={posts} />);
+  return <div></div>;
 };
 
 export default App;
