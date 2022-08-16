7cdf7d4f4df675b23ebc4298076ef07b7776b5c2
ben40d9 <benpaley6@gmail.com>
2022-08-15 18:26:02 -0700

    updated to other branch, will now try forEach instead of map. want to test something

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const App = () => {
+  const [advice, setAdvice] = useState("");
+
   useEffect(() => {
     //adding the ?_limit=X to the end of the url sets a limit on how many objects you get back
     const url = "https://jsonplaceholder.typicode.com/posts?_limit=12";
@@ -10,16 +12,19 @@ const App = () => {
       try {
         const response = await fetch(url);
         const data = await response.json();
-        //when I do not use spread syntax the data is returned in an array of objects
         console.log(data);
-        //logging (...data) show objects listed individually
-        // console.log(...data);
 
-        //using array manipuation, get params of each object in the array
-        const dataTitle = await data.map((objs) => {
+        const testy = await data.map((objs) => {
           return objs.title;
+          // console.log(objs.title);
         });
-        console.log(dataTitle);
+
+        console.log(testy);
+        // //using array manipuation, get params of each object in the array
+        // const dataTitle = await data.map((objs) => {
+        //   return objs.title;
+        // });
+        // console.log(dataTitle);
       } catch (err) {
         console.log("error", err);
       }
@@ -27,7 +32,7 @@ const App = () => {
 
     fetchData();
   }, []);
-  return <div></div>;
+  return <div>{}</div>;
 };
 
 export default App;
